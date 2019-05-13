d375be4641700232d70fc397ae7b3cb5b17c6655
David <ollangamoubambabdavid@gmail.com>
2019-05-13 11:26:35 +0200

    Premier fichier PHP

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport">
+
+    <title>Vision Voyage</title>
+</head>
+<body>
+<h1>Ma page</h1>
+
+<?php
+
+
+echo 'Bonjour';
+
+?>
+
+</body>
+</html>
